da2a53952b4372a5ba19cfe7e550eb1bee5e753b
vector <samcodebello@gmail.com>
2024-08-20 20:31:41 +0100

    added ability for player to move left, right, up, down

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from constants import PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -21,3 +22,29 @@ class Player(CircleShape):
     def draw(self, screen):
         points = self.triangle()
         pygame.draw.polygon(screen, (255, 255, 255), points, 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        # left
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        # right
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+        # down
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+        # up
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
