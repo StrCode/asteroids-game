3b74a3ed2cbf8f92108b0eb66730addd6ebd70ea
vector <samcodebello@gmail.com>
2024-08-21 19:04:55 +0100

    fixup! added asteriods and asteriods field

--- main.py
+++ main.py
@@ -2,54 +2,48 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, FRAME_RATE
-from player import Player
 
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-
-    # creates a screen surface to be used to display the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
-    # create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    asteriods = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteriods, updatable, drawable)
-    AsteroidField.containers = updatable
 
-    # create a player object
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    asterfiled = AsteroidField()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        for thing in updatable:
-            thing.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
 
-        for thing in drawable:
-            thing.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
-        dt = clock.tick(FRAME_RATE) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
